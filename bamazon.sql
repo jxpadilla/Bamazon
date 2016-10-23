9fe6a463463de9d65f9a08ba73db99d02defde9d
Juan Padilla <jxpadilla@yahoo.com>
2016-10-22 19:18:33 -0500

    on the road to finalize

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+						
+MySQL DB: Bamazon						
+						
+Table:	Products					
+		ItemID	INTEGER(4)			
+		ProductName	VARCHAR(20)			
+		DeptName	VARCHAR(30)			
+		Price	DECIMAL(10,2)			
+		StockQty	INTEGER			
+						
+						
+						
+		ItemId	ProductName		DeptName		UnitPrice	StockQty
+			1	City Bike		Road Bikes		500			30
+			2	Commuter 		Road Bikes		600			20
+			3	Cyclocross		Road Bikes		800			15
+			4	Racer			Road Bikes		1500		10
+			5	Touring			Road Bikes		1800		5
+			6	Cross Country	Mountain Bikes	700			10
+			7	Freeride		Mountain Bikes	650			20
+			8	Mountain Trail	Mountain Bikes	450			20
+			9	Folding			Specialty		300			5
+			10	Sport Hybrid	Specialty		850			25
+			11	Street Hybrid	Specialty		750			30
+			12	Kids BMX		Specialty		400			10
